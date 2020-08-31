414afc771b23452a00b5de6ead78cd6a94166522
Admin <admin@localhost>
2020-08-31 09:34:24 -0400

    Working on getting it transfering to new tiddler

--- src/tiddlers/system/plugins/huntsfromshadow/StarfinderManager/widgets/action-sf-create-from-tmp.js
+++ src/tiddlers/system/plugins/huntsfromshadow/StarfinderManager/widgets/action-sf-create-from-tmp.js
@@ -1,7 +1,7 @@
 /*\
 created: 20200830151140256
 type: application/javascript
-title: $:/plugins/huntsfromshadow/StarfinderManager/widgets/action-sf-tmp-npc.js
+title: $:/plugins/huntsfromshadow/StarfinderManager/widgets/action-sf-create-from-temp.js
 tags: 
 modified: 20200830153228079
 module-type: widget
@@ -15,22 +15,22 @@ module-type: widget
 var Widget = require("$:/core/modules/widgets/widget.js").widget;
 
 /* Creates a new <$foo> widget. */
-var SfTmpNpcWidget = function(parseTreeNode, options) {
+var SfCreateFromTmpWidget = function(parseTreeNode, options) {
   this.initialise(parseTreeNode, options);
 };
   
 /* "Inherits" from the Widget base "class" in order to get all
  * the basic widget functionality.
  */
-SfTmpNpcWidget.prototype = new Widget();
+SfCreateFromTmpWidget.prototype = new Widget();
 
-SfTmpNpcWidget.prototype.render = function(parent,nextSibling) {
+SfCreateFromTmpWidget.prototype.render = function(parent,nextSibling) {
   this.computeAttributes();
   this.execute();
 };
 
 /* Computes the internal state of this widget. */
-SfTmpNpcWidget.prototype.execute = function() {
+SfCreateFromTmpWidget.prototype.execute = function() {
   this.npcname = this.getAttribute("$npcname");
   this.rawblock = this.getAttribute("$rawblock");  
 };  
@@ -39,7 +39,7 @@ SfTmpNpcWidget.prototype.execute = function() {
  * true if either this widget itself or one of its children
  * needs to be re-rendered.
  */
-SfTmpNpcWidget.prototype.refresh = function(changedTiddlers) {
+SfCreateFromTmpWidget.prototype.refresh = function(changedTiddlers) {
   var changedAttributes = this.computeAttributes();
 	if($tw.utils.count(changedAttributes) > 0) {
 		this.refreshSelf();
@@ -48,7 +48,7 @@ SfTmpNpcWidget.prototype.refresh = function(changedTiddlers) {
   return this.refreshChildren(changedTiddlers);  
 };
 
-SfTmpNpcWidget.prototype.invokeAction = function(triggeringWidget,event) {
+SfCreateFromTmpWidget.prototype.invokeAction = function(triggeringWidget,event) {
   var import_array = [
     ["npc_name", "(.*) CR \\d{1,2}\\n"],
     ["npc_cr", ".* CR (\\d{1,2})\\n"],
@@ -135,6 +135,6 @@ SfTmpNpcWidget.prototype.invokeAction = function(triggeringWidget,event) {
 
 /* Finally exports the widget constructor. */
 //exports.foo = FooWidget;
-exports["action-sf-tmp-npc"] = SfTmpNpcWidget;
+exports["action-sf-create-from-temp"] = SfCreateFromTmpWidget;
 
 })();
